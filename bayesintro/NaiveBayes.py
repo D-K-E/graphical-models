982903650b27f94c2ddb21be4d3cc60bef6774eb
Qm Auber <qm.auber@tutanota.com>
2020-12-16 03:03:09 +0100

    BasicNaiveBayes done
    Basic naive bayes done which introduces concepts.
    It needs a little bit of documentation maybe but that's
    about it.
    Now to NaiveBayes classifier which abstracts away
    concepts introduced in BasicNaiveBayes

---
+++ bayesintro/NaiveBayes.py
@@ -0,0 +1,13 @@
+"""
+Main object that abstracts away concepts
+introduced in BasicNaiveBayes.py
+"""
+
+import numpy as np
+import pandas as pd
+
+class NaiveBayes:
+    """
+    """
+    def __init__(self):
+        pass
