ebe6147cf7e7c5d9ded0f89cfc5814a220140261
Qm Auber <qm.auber@tutanota.com>
2021-01-24 02:57:38 +0100

    product and reduce value truly tested

--- test/test_factor.py
+++ test/test_factor.py
@@ -76,6 +76,135 @@ class TestFactor(unittest.TestCase):
         )
         self.f2 = Factor(gid="f2", scope_vars=set([self.grade, self.fdice]))
 
+        # Koller, Friedman 2009, p. 104
+        self.Af = NumCatRVariable(
+            node_id="A",
+            input_data={"outcome-values": [10, 50]},
+            distribution=lambda x: 0.5,
+        )
+        self.Bf = NumCatRVariable(
+            node_id="B",
+            input_data={"outcome-values": [10, 50]},
+            distribution=lambda x: 0.5,
+        )
+        self.Cf = NumCatRVariable(
+            node_id="C",
+            input_data={"outcome-values": [10, 50]},
+            distribution=lambda x: 0.5,
+        )
+        self.Df = NumCatRVariable(
+            node_id="D",
+            input_data={"outcome-values": [10, 50]},
+            distribution=lambda x: 0.5,
+        )
+
+        def phiAB(scope_product):
+            ""
+            sfs = set(scope_product)
+            if sfs == set([("A", 10), ("B", 10)]):
+                return 30
+            elif sfs == set([("A", 10), ("B", 50)]):
+                return 5
+            elif sfs == set([("A", 50), ("B", 10)]):
+                return 1
+            elif sfs == set([("A", 50), ("B", 50)]):
+                return 10
+            else:
+                raise ValueError("unknown arg")
+
+        self.AB = Factor(gid="AB", scope_vars=set([self.Af, self.Bf]), factor_fn=phiAB)
+
+        def phiBC(scope_product):
+            ""
+            sfs = set(scope_product)
+            if sfs == set([("B", 10), ("C", 10)]):
+                return 100
+            elif sfs == set([("B", 10), ("C", 50)]):
+                return 1
+            elif sfs == set([("B", 50), ("C", 10)]):
+                return 1
+            elif sfs == set([("B", 50), ("C", 50)]):
+                return 100
+            else:
+                raise ValueError("unknown arg")
+
+        self.BC = Factor(gid="BC", scope_vars=set([self.Bf, self.Cf]), factor_fn=phiBC)
+
+        def phiCD(scope_product):
+            ""
+            sfs = set(scope_product)
+            if sfs == set([("C", 10), ("D", 10)]):
+                return 1
+            elif sfs == set([("C", 10), ("D", 50)]):
+                return 100
+            elif sfs == set([("C", 50), ("D", 10)]):
+                return 100
+            elif sfs == set([("C", 50), ("D", 50)]):
+                return 1
+            else:
+                raise ValueError("unknown arg")
+
+        self.CD = Factor(gid="CD", scope_vars=set([self.Cf, self.Df]), factor_fn=phiCD)
+
+        def phiDA(scope_product):
+            ""
+            sfs = set(scope_product)
+            if sfs == set([("D", 10), ("A", 10)]):
+                return 100
+            elif sfs == set([("D", 10), ("A", 50)]):
+                return 1
+            elif sfs == set([("D", 50), ("A", 10)]):
+                return 1
+            elif sfs == set([("D", 50), ("A", 50)]):
+                return 100
+            else:
+                raise ValueError("unknown arg")
+
+        self.DA = Factor(gid="DA", scope_vars=set([self.Df, self.Af]), factor_fn=phiDA)
+
+        # Koller, Friedman 2009 p. 107
+        self.af = NumCatRVariable(
+            node_id="A",
+            input_data={"outcome-values": [10, 50, 20]},
+            distribution=lambda x: 0.4 if x != 20 else 0.2,
+        )
+
+        def phiaB(scope_product):
+            ""
+            sfs = set(scope_product)
+            if sfs == set([("A", 10), ("B", 10)]):
+                return 0.5
+            elif sfs == set([("A", 10), ("B", 50)]):
+                return 0.8
+            elif sfs == set([("A", 50), ("B", 10)]):
+                return 0.1
+            elif sfs == set([("A", 50), ("B", 50)]):
+                return 0
+            elif sfs == set([("A", 20), ("B", 10)]):
+                return 0.3
+            elif sfs == set([("A", 20), ("B", 50)]):
+                return 0.9
+            else:
+                raise ValueError("unknown arg")
+
+        self.aB = Factor(gid="ab", scope_vars=set([self.af, self.Bf]), factor_fn=phiaB)
+
+        def phibc(scope_product):
+            ""
+            sfs = set(scope_product)
+            if sfs == set([("B", 10), ("C", 10)]):
+                return 0.5
+            elif sfs == set([("B", 10), ("C", 50)]):
+                return 0.7
+            elif sfs == set([("B", 50), ("C", 10)]):
+                return 0.1
+            elif sfs == set([("B", 50), ("C", 50)]):
+                return 0.2
+            else:
+                raise ValueError("unknown arg")
+
+        self.bc = Factor(gid="bc", scope_vars=set([self.Bf, self.Cf]), factor_fn=phibc)
+
     def test_id(self):
         ""
         self.assertEqual(self.f.id(), "f")
@@ -124,27 +253,97 @@ class TestFactor(unittest.TestCase):
         self.assertTrue(mjoint, (dmarg * imarg * gmarg) / self.f.zval())
 
     def test_factor_product(self):
-        ""  # TODO needs a better test case
-        f3 = self.f.product(
-            self.f2,
-            product_fn=lambda x, y: round(x * y, 8),
-            accumulator=lambda x, y: round(x * y, 7),
-        )
+        "from Koller, Friedman 2009, p. 105, figure 4.2"
+        Ab_Bc, prod1 = self.AB.product(self.BC)
+        Ab_Bc_Cd, prod2 = Ab_Bc.product(self.CD)
+        result, prod3 = Ab_Bc_Cd.product(self.DA)
+        for sm in result.scope_products:
+            sms = set(sm)
+            f = result.phi(sms)
+            ff = round(result.phi_normal(sms), 6)
+            if sms == set([("B", 50), ("C", 50), ("A", 50), ("D", 10)]):
+                self.assertEqual(f, 100000)
+                self.assertEqual(ff, 0.013885)
+
+            elif sms == set([("B", 50), ("C", 50), ("A", 50), ("D", 50)]):
+                self.assertEqual(f, 100000)
+                self.assertEqual(ff, 0.013885)
+
+            elif sms == set([("B", 50), ("C", 50), ("A", 10), ("D", 10)]):
+                self.assertEqual(f, 5000000)
+                self.assertEqual(ff, 0.694267)
+
+            elif sms == set([("B", 50), ("C", 50), ("A", 10), ("D", 50)]):
+                self.assertEqual(f, 500)
+                self.assertEqual(ff, 6.9e-05)
+
+            elif sms == set([("B", 50), ("C", 10), ("A", 50), ("D", 10)]):
+                self.assertEqual(f, 10)
+                self.assertEqual(ff, 1e-06)
+
+            elif sms == set([("B", 50), ("C", 10), ("A", 50), ("D", 50)]):
+                self.assertEqual(f, 100000)
+                self.assertEqual(ff, 0.013885)
+            elif sms == set([("B", 50), ("C", 10), ("A", 10), ("D", 10)]):
+                self.assertEqual(f, 500)
+                self.assertEqual(ff, 6.9e-05)
+
+            elif sms == set([("B", 50), ("C", 10), ("A", 10), ("D", 50)]):
+                self.assertEqual(f, 500)
+                self.assertEqual(ff, 6.9e-05)
+
+            elif sms == set([("B", 10), ("C", 50), ("A", 50), ("D", 10)]):
+                self.assertEqual(f, 100)
+                self.assertEqual(ff, 1.4e-05)
+
+            elif sms == set([("B", 10), ("C", 50), ("A", 50), ("D", 50)]):
+                self.assertEqual(f, 100)
+                self.assertEqual(ff, 1.4e-05)
+            elif sms == set([("B", 10), ("C", 50), ("A", 10), ("D", 10)]):
+                self.assertEqual(f, 300000)
+                self.assertEqual(ff, 0.041656)
+
+            elif sms == set([("B", 10), ("C", 50), ("A", 10), ("D", 50)]):
+                self.assertEqual(f, 30)
+                self.assertEqual(ff, 4e-06)
+
+            elif sms == set([("B", 10), ("C", 10), ("A", 50), ("D", 10)]):
+                self.assertEqual(f, 100)
+                self.assertEqual(ff, 1.4e-05)
+
+            elif sms == set([("B", 10), ("C", 10), ("A", 50), ("D", 50)]):
+                self.assertEqual(f, 1000000)
+                self.assertEqual(ff, 0.138853)
+
+            elif sms == set([("B", 10), ("C", 10), ("A", 10), ("D", 10)]):
+                self.assertEqual(f, 300000)
+                self.assertEqual(ff, 0.041656)
+            elif sms == set([("B", 10), ("C", 10), ("A", 10), ("D", 50)]):
+                self.assertEqual(f, 300000)
+                self.assertEqual(ff, 0.041656)
 
     def test_reduced_by_value(self):
         ""
-        red = set([("dice", 3), ("grade", 0.4)])
-        self.f.reduced_by_value(context=red)
-        com = sum(
-            [
-                self.f.factor_fn(f)
-                for f in [
-                    {("dice", 3), ("int", 0.1), ("grade", 0.4)},
-                    {("dice", 3), ("int", 0.9), ("grade", 0.4)},
-                ]
-            ]
-        )
-        self.assertEqual(com, self.f.Z)
+        red = set([("C", 10)])
+        aB_c, prod = self.aB.product(self.bc)
+        # print(aB_c.scope_products)
+        aB_c.reduced_by_value(context=red)
+        # print(aB_c.scope_products)
+        for p in aB_c.scope_products:
+            ps = set(p)
+            f = round(aB_c.phi(ps), 5)
+            if ps == set([("A", 10), ("B", 50), ("C", 10)]):
+                self.assertEqual(f, 0.08)
+            elif ps == set([("A", 10), ("B", 10), ("C", 10)]):
+                self.assertEqual(f, 0.25)
+            elif ps == set([("B", 50), ("A", 20), ("C", 10)]):
+                self.assertEqual(f, 0.09)
+            elif ps == set([("B", 10), ("A", 20), ("C", 10)]):
+                self.assertEqual(f, 0.15)
+            elif ps == set([("A", 50), ("B", 50), ("C", 10)]):
+                self.assertEqual(f, 0.0)
+            elif ps == set([("A", 50), ("B", 10), ("C", 10)]):
+                self.assertEqual(f, 0.05)
 
     def test_reduce_by_vars(self):
         ""
@@ -158,4 +357,5 @@ class TestFactor(unittest.TestCase):
     def test_sumout_var(self):
         ""
         nf = self.f.sumout_var(self.grade)
-        self.assertEqual(round(nf), 1.0)
+        print(self.f.Z)
+        print(nf.Z)
