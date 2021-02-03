6b9c4823738625b686ad3e11b6f04639e189d482
Qm Auber <qm.auber@tutanota.com>
2021-02-03 03:01:02 +0100

    doxygen documentation generated

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,12 +1,33 @@
 var indexSectionsWithContent =
 {
+  0: "_abcdefghijklmnoprstuv",
+  1: "abcdefglmnoprstu",
+  2: "g",
+  3: "aegnp",
+  4: "_abcdefghijkmnoprstuv",
+  5: "g",
+  6: "g"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "groups",
+  6: "pages"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Modules",
+  6: "Pages"
 };
 
