dd4ec5b82753b474df2b5ed28f67bb2dd274aa63
Qm Auber <qm.auber@tutanota.com>
2021-04-29 05:08:01 +0200

    doc update

--- docs/html/search/all_14.js
+++ docs/html/search/all_14.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['undigraph_260',['UndiGraph',['../d8/d73/classgmodels_1_1gtypes_1_1undigraph_1_1UndiGraph.html',1,'gmodels::gtypes::undigraph']]],
-  ['undigraph_2epy_261',['undigraph.py',['../d7/daa/undigraph_8py.html',1,'']]],
-  ['uniform_5fcost_5fsearch_262',['uniform_cost_search',['../d0/d1c/classgmodels_1_1gtypes_1_1path_1_1Path.html#a77a304703d7b511f6245fa2554ccc273',1,'gmodels::gtypes::path::Path']]],
-  ['union_263',['union',['../d2/dfc/classgmodels_1_1gtypes_1_1graph_1_1Graph.html#a2ee3e394b96067bec663d665f7438ae8',1,'gmodels::gtypes::graph::Graph']]],
-  ['upset_5fof_264',['upset_of',['../d7/db3/classgmodels_1_1gtypes_1_1tree_1_1Tree.html#ac51409a988123ac84c6d210fa48d4d9e',1,'gmodels::gtypes::tree::Tree']]],
-  ['usage_20examples_265',['Usage Examples',['../da/d1b/usage.html',1,'']]]
+  ['usage_20examples_260',['Usage Examples',['../d3/db0/md_graphical-models_docs_mdpages_usage.html',1,'']]],
+  ['undigraph_261',['UndiGraph',['../d8/d73/classgmodels_1_1gtypes_1_1undigraph_1_1UndiGraph.html',1,'gmodels::gtypes::undigraph']]],
+  ['undigraph_2epy_262',['undigraph.py',['../d7/daa/undigraph_8py.html',1,'']]],
+  ['uniform_5fcost_5fsearch_263',['uniform_cost_search',['../d0/d1c/classgmodels_1_1gtypes_1_1path_1_1Path.html#a77a304703d7b511f6245fa2554ccc273',1,'gmodels::gtypes::path::Path']]],
+  ['union_264',['union',['../d2/dfc/classgmodels_1_1gtypes_1_1graph_1_1Graph.html#a2ee3e394b96067bec663d665f7438ae8',1,'gmodels::gtypes::graph::Graph']]],
+  ['upset_5fof_265',['upset_of',['../d7/db3/classgmodels_1_1gtypes_1_1tree_1_1Tree.html#ac51409a988123ac84c6d210fa48d4d9e',1,'gmodels::gtypes::tree::Tree']]]
 ];
