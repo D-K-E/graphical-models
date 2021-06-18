2be577bb212862a025b565ec8cdc120571347120
Qm Auber <qm.auber@tutanota.com>
2021-06-19 00:07:07 +0200

    fixed analyzer error

--- gmodels/gtypes/analyzer.py
+++ gmodels/gtypes/analyzer.py
@@ -4,6 +4,7 @@ graph analyzer
 """
 from typing import Set, Optional, Callable, List, Tuple
 from gmodels.gtypes.graph import Graph
+from gmodels.gtypes.graphobj import AbstractGraph
 from gmodels.gtypes.finitegraph import FiniteGraph
 from gmodels.gtypes.basegraph import BaseGraph
 from gmodels.gtypes.node import Node
