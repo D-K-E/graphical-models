77e43e66ee9f73bb89a2356f107b472974398c28
Qm Auber <qm.auber@tutanota.com>
2021-07-29 01:31:07 +0200

    Heavy refactoring in abstract objects
    Abstract objects now type check some of their
    abstractmethods. Basically they call some of the
    abstract methods in check_types function which
    checks each method result for its type.
    The check_types method then is called in
    the constructor of the abstract class.
    That way it is checked for all subclassing
    objects.
    
    The same idea should be implemented for abstractfactor
    and abstractpgm objects.

--- gmodels/gtype/abstractobj.py
+++ gmodels/gtype/abstractobj.py
@@ -10,13 +10,32 @@ from copy import deepcopy
 
 
 class AbstractInfo(ABC):
+    ""
+
+    def __init__(self, *args, **kwargs):
+        ""
+        self.check_types()
+
     @abstractmethod
     def id(self) -> str:
         raise NotImplementedError
 
+    def check_types(self) -> bool:
+        ""
+        ival = self.id()
+        if isinstance(ival, str) is False:
+            itype = str(type(ival))
+            mes = "id() method must return str as type it returns " + itype
+            raise TypeError(mes)
+        return True
+
 
 class AbstractGraphObj(AbstractInfo):
     "Abstract graph object"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.check_types()
 
     @abstractmethod
     def data(self) -> Dict:
@@ -38,6 +57,25 @@ class AbstractGraphObj(AbstractInfo):
     def __hash__(self):
         raise NotImplementedError
 
+    def check_types(self) -> bool:
+        ""
+        s = self.__str__()
+        b = self.__eq__()
+        d = self.data()
+        if isinstance(s, str) is False:
+            itype = str(type(s))
+            mes = "__str__() method must return str as type it returns " + itype
+            raise TypeError(mes)
+        if isinstance(b, bool) is False:
+            itype = str(type(b))
+            mes = "__eq__() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+        if isinstance(d, dict) is False:
+            itype = str(type(d))
+            mes = "data() method must return dict as type it returns " + itype
+            raise TypeError(mes)
+        return True
+
 
 class EdgeType(Enum):
     DIRECTED = 1
@@ -48,19 +86,24 @@ class AbstractNode(AbstractGraphObj):
     ""
 
 
-class AbstractEdge(ABC):
+class AbstractEdge(AbstractGraphObj):
     "abstract edge object"
+
+    def __init__(self, *args, **kwargs):
+        ""
+        super().__init__(*args, **kwargs)
+        self.check_types()
 
     @abstractmethod
     def type(self) -> EdgeType:
         raise NotImplementedError
 
     @abstractmethod
-    def is_start(self):
+    def is_start(self) -> bool:
         raise NotImplementedError
 
     @abstractmethod
-    def is_end(self):
+    def is_end(self) -> bool:
         raise NotImplementedError
 
     @abstractmethod
@@ -69,6 +112,9 @@ class AbstractEdge(ABC):
 
     @abstractmethod
     def get_other(self, n: Union[AbstractNode, str]) -> AbstractNode:
+        """!
+        \todo Type checking is not done
+        """
         raise NotImplementedError
 
     @abstractmethod
@@ -83,11 +129,66 @@ class AbstractEdge(ABC):
     def end(self) -> AbstractNode:
         raise NotImplementedError
 
+    def check_types(self) -> bool:
+        ""
+        tv = self.type()
+        is_start_b = self.is_start("f")
+        is_end_b = self.is_end("f")
+        node_ids = self.node_ids()
+        sval = self.start()
+        endv = self.end()
+        ivert = self.is_endvertice("fre")
+        if isinstance(tv, EdgeType) is False:
+            itype = str(type(tv))
+            mes = "type() method must return EdgeType as type it returns " + itype
+            raise TypeError(mes)
+        if isinstance(is_start_b, bool) is False:
+            itype = str(type(is_start_b))
+            mes = "is_start() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+        if isinstance(is_end_b, bool) is False:
+            itype = str(type(is_end_b))
+            mes = "is_end() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+        if isinstance(node_ids, frozenset) is False:
+            itype = str(type(node_ids))
+            mes = "node_ids() method must return frozenset as type it returns " + itype
+            raise TypeError(mes)
+        n = set(node_ids).pop()
+        if isinstance(n, str) is False:
+            itype = str(type(n))
+            mes = (
+                "node_ids() method must return frozenset whose members are"
+                + "string as type it returns "
+                + itype
+            )
+            raise TypeError(mes)
+
+        if isinstance(ivert, bool) is False:
+            itype = str(type(ivert))
+            mes = "is_end() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+
+        if isinstance(sval, AbstractNode) is False:
+            itype = str(type(sval))
+            mes = "start() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+
+        if isinstance(endv, AbstractNode) is False:
+            itype = str(type(endv))
+            mes = "start() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+        return True
+
 
 class AbstractGraph(AbstractGraphObj):
     """!
     \brief Abstract Graph interface
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.check_types()
 
     @property
     @abstractmethod
@@ -109,4 +210,47 @@ class AbstractGraph(AbstractGraphObj):
 
     @abstractmethod
     def is_neighbour_of(self, n1: AbstractNode, n2: AbstractNode) -> bool:
+        """!
+        \todo type checking not done in check types
+        """
         raise NotImplementedError
+
+    def check_types(self) -> bool:
+        ""
+        vtypes = all(
+            [
+                isinstance(vid, str) and isinstance(v, AbstractNode)
+                for vid, v in self.V.items()
+            ]
+        )
+        etypes = all(
+            [
+                isinstance(vid, str) and isinstance(v, AbstractEdge)
+                for vid, v in self.E.items()
+            ]
+        )
+        itriv = self.is_trivial()
+        iorder = self.order()
+        if vtypes is False:
+            mes = "self.V property must return Dict[str, AbstractNode] it fails "
+            mes += " for the following test:\n"
+            mes += "[isinstance(vid, str) and isinstance(v, AbstractNode) "
+            mes += "for vid, v in self.V.items()]"
+            raise TypeError(mes)
+
+        if etypes is False:
+            mes = "self.E property must return Dict[str, AbstractEdge] it fails "
+            mes += " for the following test:\n"
+            mes += "[isinstance(vid, str) and isinstance(v, AbstractEdge) "
+            mes += "for vid, v in self.E.items()]"
+            raise TypeError(mes)
+
+        if isinstance(itriv, bool) is False:
+            itype = str(type(itriv))
+            mes = "is_trivial() method must return bool as type it returns " + itype
+            raise TypeError(mes)
+
+        if isinstance(iorder, int) is False:
+            itype = str(type(iorder))
+            mes = "order() method must return int as type it returns " + itype
+            raise TypeError(mes)
