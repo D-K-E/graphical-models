e2937075ad76622203130ef6c8c387dd8ecc10aa
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2021-10-07 23:07:38 +0200

    digraphops and test written

--- pygmodels/graphf/digraphops.py
+++ pygmodels/graphf/digraphops.py
@@ -10,6 +10,7 @@ from pygmodels.gtype.abstractobj import AbstractDiGraph
 from pygmodels.gtype.abstractobj import AbstractNode
 from pygmodels.gtype.abstractobj import AbstractEdge
 from pygmodels.graphf.bgraphops import BaseGraphBoolOps
+from typing import Set, Callable
 
 
 class DiGraphBoolOps:
@@ -56,7 +57,7 @@ class DiGraphBoolOps:
         v and incoming edge of w than v is parent of w.
         """
 
-        def cond(n_1: Node, n_2: Node, e: Edge):
+        def cond(n_1: AbstractNode, n_2: AbstractNode, e: AbstractEdge):
             """"""
             c = n_1 == e.start() and e.end() == n_2
             return c
@@ -77,3 +78,115 @@ class DiGraphBoolOps:
         if v is child of w, then w is parent of v.
         """
         return DiGraphBoolOps.is_parent_of(g, parent=parent, child=child)
+
+    @staticmethod
+    def is_adjacent_of(g: AbstractDiGraph, e1: AbstractEdge, e2: AbstractEdge) -> bool:
+        """!
+        \brief check if edges have a common node
+        """
+        if not BaseGraphBoolOps.is_in(self, e1) or not BaseGraphBoolOps.is_in(self, e2):
+            raise ValueError("argument edges are not in graph")
+
+        n1_ids = e1.node_ids()
+        n2_ids = e2.node_ids()
+        return len(n1_ids.intersection(n2_ids)) > 0
+
+
+class DiGraphNumericOps:
+    """!
+    Operations that result in numeric results involving directed graphs
+    """
+
+    @staticmethod
+    def in_degree_of(g: AbstractDiGraph, n: AbstractNode) -> int:
+        return len(DiGraphNodeOps.parents_of(g, n))
+
+    @staticmethod
+    def out_degree_of(g: AbstractDiGraph, n: AbstractNode) -> int:
+        return len(DiGraphNodeOps.children_of(g, n))
+
+
+class DiGraphEdgeOps:
+    """!
+    Operations that result in edge or set of edges with directed graphs
+    """
+
+    @staticmethod
+    def edge_by_vertices(
+        g: AbstractDiGraph, start: AbstractNode, end: AbstractNode
+    ) -> Set[AbstractEdge]:
+        """!
+        \brief obtain edge by using its start and end node.
+
+        \throws ValueError If any of the arguments are not found in this graph we
+        throw value error.
+        """
+        if not BaseGraphBoolOps.is_in(self, start) or not BaseGraphBoolOps.is_in(
+            self, end
+        ):
+            raise ValueError("argument nodes are not in graph")
+        #
+        eset: Set[Edge] = set()
+        for e in g.E:
+            if e.start().id() == start.id() and e.end().id() == end.id():
+                eset.add(e)
+        return eset
+
+
+class DiGraphNodeOps:
+    """!
+    Operations that result in node or set of nodes with directed graphs
+    """
+
+    @staticmethod
+    def family_set_of(
+        g: AbstractDiGraph,
+        n: AbstractNode,
+        fcond: Callable[[AbstractEdge, AbstractNode], bool],
+        enode_fn: Callable[[AbstractEdge], AbstractNode],
+    ):
+        """!
+        \brief obtain direct family set of nodes from given argument
+
+        \param n argument node whose family set, we are interested in
+        \param fcond family condition function
+        \param enode_fn node extracting function. Extracts node from given edge
+
+        \throws ValueError if the argument does not belong to this graph we
+        throw value error.
+        """
+        if not BaseGraphBoolOps.is_in(g, n):
+            raise ValueError("node not in graph")
+        family = set()
+        for e in g.E:
+            if fcond(e, n) is True:
+                family.add(enode_fn(e))
+        return family
+
+    def children_of(g: AbstractDiGraph, n: AbstractNode) -> Set[AbstractNode]:
+        """!
+        \brief obtain direct child set of nodes from given argument
+
+        \throws ValueError if the argument does not belong to this graph we
+        throw value error.
+        """
+        return DiGraphNodeOps.family_set_of(
+            g=g,
+            n=n,
+            fcond=lambda e, node: e.start().id() == node.id(),
+            enode_fn=lambda e: e.end(),
+        )
+
+    def parents_of(g: AbstractDiGraph, n: AbstractNode) -> Set[AbstractNode]:
+        """!
+        \brief obtain direct parent set of nodes from given argument
+
+        \throws ValueError if the argument does not belong to this graph we
+        throw value error.
+        """
+        return DiGraphNodeOps.family_set_of(
+            g=g,
+            n=n,
+            fcond=lambda e, node: e.end().id() == node.id(),
+            enode_fn=lambda e: e.start(),
+        )
