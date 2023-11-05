98c71ef4999bfb42cb2bb8bc0b9c60cc27974faf
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2023-11-05 17:34:06 +0900

    moved tests and removed typeutils

--- pygmodels/graph/graphtype/typeutils.py
+++
@@ -1,4 +0,0 @@
-# type related utility functions
-from pygmodels.graphtypes.abstractobj import AbstractGraph
-from pygmodels.graphtypes.basegraph import BaseGraph
-from pygmodels.graphtypes.finitegraph import FiniteGraph
---
+++ test/graph/graphtype/test_gsearchresult.py
@@ -0,0 +1,3 @@
+"""
+gsearchresult.py tests
+"""
